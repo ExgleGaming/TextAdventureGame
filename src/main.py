5267ce31fb78cfb4f887419f07a2b779a2746b2a
HunterJones <eaglesfan91x@outlook.com>
2024-05-29 20:52:58 -0400

    Start of creating the game

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 def main():
-    print("Hello Text Adventurers!")
-
+    print("Hello and welcom to the Adventure Game!")
+    print("Are you ready to take an adventure in this cave?")
+    answer = input("Yes or No: ")
+    if answer.lower() == "yes":
+        pass
+    elif answer.lower() == "no":
+        return 0
+    else:
+        print("Please enter a valid input.")
+    
 main()
+    
 
